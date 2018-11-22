2802f9aac77d73787b5b0d66b0ffa9f76b6040cf
SuiteLab <yoonwoosik12@naver.com>
2018-11-22 19:20:22 +0900

    add chrome option

--- crawler.py
+++ crawler.py
@@ -26,6 +26,7 @@ class Crawler:
         options.add_argument('headless')
         options.add_argument('disable-gpu')
         options.add_argument('no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
 
         chrome_path = ''
         if platform.system() == "Linux":
